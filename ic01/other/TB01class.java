fdd5525963e0540055208a2cafb27c022b02361e
Phill Conrad <pconrad@cs.ucsb.edu>
2018-01-30 12:01:53 -0800

    restore TB01class

---
+++ ic01/other/TB01class.java
@@ -0,0 +1,12 @@
+public class TB01class {
+
+	public static void TB01 () {
+		Beverage m = new Beverage(99,"Coke",150,12.0);
+		System.out.println("m: " + m.getCalories());
+	}
+	
+	public static void main (String [] args) {
+		TB01();
+	}
+						
+} 
